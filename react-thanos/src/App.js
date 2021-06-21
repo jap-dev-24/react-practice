4e52a9c3a601181b2dd87d84a8363b888b24b06f
Jap <jap.baldera@ph.gt.com>
2021-06-21 21:30:54 +0800

    COnditional javascript way of showing and hiding element

--- react-thanos/src/App.js
+++ react-thanos/src/App.js
@@ -52,6 +52,33 @@ class App extends Component {
       cursor: "pointer",
     };
 
+    let persons =null;
+
+    if (this.state.showPersons) {
+      persons = (
+        
+          <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          ></Person>
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, "Max!")}
+            changed={this.nameChangedHandler}
+          >
+            My Hobbies: Racing
+          </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          ></Person>
+        </div>
+        
+      );
+    } 
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
@@ -59,26 +86,11 @@ class App extends Component {
         <button style={style} onClick={this.togglePersonsHandler}>
           Toggle Person
         </button>
-        {this.state.showPersons === true ? (
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-            ></Person>
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, "Max!")}
-              changed={this.nameChangedHandler}
-            >
-              My Hobbies: Racing
-            </Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-            ></Person>
-          </div>
-        ) : null}
+
+        {persons}
+     
+        
+
       </div>
     );
   }
