359f663a7ce3fab34a3e941b464ce1bd0a3e3302
Jap <jap.baldera@ph.gt.com>
2021-06-21 19:24:23 +0800

    Day 3

--- react-thanos/Rollback/App-JS-Back-up-function-component.js
+++ react-thanos/Rollback/App-JS-Back-up-function-component.js
@@ -1,65 +1,64 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-class App extends Component {
-  state = {
+const App = (props) => {
+
+  //constant declaration
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: "Max", age: 28 },
       { name: "Manu", age: 29 },
       { name: "Stephanie", age: 26 },
-    ],
-    otherState: "some other value",
-  };
+    ]
+  });
 
-  switchNameHandler = (newName) => {
+  const [otherState,setOtherState] = useState('some other value');
+
+
+
+  console.log(personsState,otherState);
+
+  //function for manipulation
+
+  const switchNameHandler = () => {
     // console.log('Was clicked!');
     //DONT DO THIS this.state.persons[0].name = 'Maximillian';
-    this.setState({
+    setPersonsState({
       persons: [
-        { name: newName, age: 28 },
+        { name: "Maximilian", age: 28 },
         { name: "Manu", age: 29 },
         { name: "Stephanie", age: 27 },
-      ],
+      ]
+    });
+
+    setOtherState({
+      otherState:'Test it went'
     });
   };
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: "Max", age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: "Stephanie", age: 26 },
-      ],
-    });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('Maximillian!!')}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        ></Person>
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this,'Max!')}
-          changed={this.nameChangedHandler}
-        >
-          My Hobbies: Racing
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        ></Person>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p>This is really working!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      ></Person>
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      >
+        My Hobbies: Racing
+      </Person>
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}
+      ></Person>
+    </div>
+  );
+};
 
 // return React.createElement('div',{className:'App'}, React.createElement('h1',null,'Does this work now?'))
 
