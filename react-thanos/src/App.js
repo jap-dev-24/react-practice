9c6aba3542320c8ac808201cac67ac5c461d1635
Jap <jap.baldera@ph.gt.com>
2021-06-21 22:14:16 +0800

    fix input box change person

--- react-thanos/src/App.js
+++ react-thanos/src/App.js
@@ -5,9 +5,9 @@ import Person from "./Person/Person";
 class App extends Component {
   state = {
     persons: [
-      { id:1, name: "Max", age: 28 },
-      { id:2 , name: "Manu", age: 29 },
-      { id:3 ,name: "Stephanie", age: 26 },
+      { id: 1, name: "Max", age: 28 },
+      { id: 2, name: "Manu", age: 29 },
+      { id: 3, name: "Stephanie", age: 26 },
     ],
     otherState: "some other value",
     showPersons: false,
@@ -25,13 +25,22 @@ class App extends Component {
   //   });
   // };
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
+    });
+
+    const person = { ...this.state.persons[personIndex] };
+
+    // const person =  Object.assign({},this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        { name: "Max", age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: "Stephanie", age: 26 },
-      ],
+      persons: persons,
     });
   };
 
@@ -40,10 +49,9 @@ class App extends Component {
 
     //update state in immutable process (create new object based of the object to be referenced of changed)
     const persons = [...this.state.persons];
-    persons.splice(personIndex,1);
-    this.setState({persons:persons});
-
-  }
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
@@ -67,15 +75,17 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map((person,index) => {
-            return <Person 
-            click={() => this.deletePersonHandler(index)}
-            name={person.name} 
-            age={person.age}
-            key={person.id}
-            />;
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
+              />
+            );
           })}
-          
         </div>
       );
     }
@@ -143,8 +153,7 @@ export default App;
 //   );
 // }
 
-
-{/* <Person
+/* <Person
             name={this.state.persons[0].name}
             age={this.state.persons[0].age}
           ></Person>
@@ -159,4 +168,4 @@ export default App;
           <Person
             name={this.state.persons[2].name}
             age={this.state.persons[2].age}
-          ></Person> */}
+          ></Person> */
